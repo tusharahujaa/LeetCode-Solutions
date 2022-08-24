b80cc958a94f3e7418336cd5dc749ad3d97d0c0a
tusharahujaa <83968279+tusharahujaa@users.noreply.github.com>
2022-08-25 00:04:19 +0530

    Time: 3 ms (46.61%), Space: 6 MB (99.16%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+       int found = haystack.find(needle);
+    if (found != string::npos)
+        return found;
+    return -1;}
+};
